5ad886c1060a9b004f29178e86bd2e52b812ef58
BORA BIOS <enzoffelipe001@gmail.com>
2026-03-17 10:34:04 -0300

    Enhance Estudante class with attributes and methods
    Added fields for student attributes and methods to get/set name.

--- EnzoScottini/src/main/java/ifc/enzoscottini/Estudante.java
+++ EnzoScottini/src/main/java/ifc/enzoscottini/Estudante.java
@@ -4,13 +4,34 @@
 
 package ifc.enzoscottini;
 
+import java.util.Date;
+
 /**
  *
  * @author aluno
  */
 public class Estudante {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    private static String nome;
+    private Date dataNascimento;
+    char genero;
+    private String matricula;
+    private String email;
+    
+        public Estudante(){
+                this.nome= "nobody";
+            
+        }
+    
+        public int obterIdade (Date hoje){
+        int idade = 0;
+        //Lógica para calcular idade
+        return idade;
     }
+        public static String getNome(){
+            return nome;
+        }
+        public static void setNome (String _nome){
+            nome = _nome;
+        }
+
 }
